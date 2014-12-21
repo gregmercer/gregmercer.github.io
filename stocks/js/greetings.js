091e7279a51d5c1d0fdd73395d8055fa16368df4
Greg Mercer (GBear) <gmercer@stanford.edu>
2014-12-21 06:51:07 -0800

    testing

--- stocks/js/greetings.js
+++ stocks/js/greetings.js
@@ -1,4 +1,4 @@
-var greetingsModule = angular.module( 'greetings', [] );
+var greetingsModule = angular.module( 'greetingsService', [] );
 
 greetingsModule.service('greetingsService', function(){
   this.sayHello= function(text){
