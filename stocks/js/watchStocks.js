5bbf9e5c459ee698a100feb6f1f0a13fdf10f991
Greg Mercer (GBear) <gmercer@stanford.edu>
2014-12-28 17:54:34 -0800

    testing

--- stocks/js/watchStocks.js
+++ stocks/js/watchStocks.js
@@ -4,6 +4,10 @@ watchModule.service('watchStocksService', function(){
   this.getData= function(){
     var symbols = [
       {symbol: "AAPL"},
+      {symbol: "GOOG"},
+      {symbol: "ORCL"},
+      {symbol: "NOV"},
+      {symbol: "HIBB"},
       {symbol: "IBM"}
     ];
     return symbols;
