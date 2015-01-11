ea252775811873445b6f17592847c654bdeff33a
Greg Mercer (GBear) <gmercer@stanford.edu>
2015-01-11 03:45:05 -0800

    Cleaning up the enddate query string code

--- stocks/js/charts.js
+++ stocks/js/charts.js
@@ -57,8 +57,6 @@ chartsModule.service('chartsService', function($http, $q) {
     var SELECT_QUERY_PART3 =
       "%26countrycode%3DUS%26time%3D8%26startdate%3D1%252F4%252F1999";
     var SELECT_QUERY_PART4 =
-      "%26enddate%3D1%252F10%252F2015";
-    var SELECT_QUERY_PART5 =
       "%26freq%3D1%26compidx%3Dnone%26compind%3Dnone%26comptemptext%3DEnter%2BSymbol%2528s%2529%26comp%3Dnone%26uf%3D0%26ma%3D1%26maval%3D200%26lf%3D1%26lf2%3D16777216%26lf3%3D33554432%26type%3D2%26size%3D2%26style%3D1013'%20&format=json&diagnostics=true&callback=";
 
     // Funds/Etfs have a slightly different query
@@ -70,8 +68,6 @@ chartsModule.service('chartsService', function($http, $q) {
       SELECT_QUERY_PART3 =
         "%26countrycode%3DUS%26time%3D8%26startdate%3D1%252F4%252F1999";
       var SELECT_QUERY_PART4 =
-        "%26enddate%3D1%252F10%252F2015";
-      var SELECT_QUERY_PART5 =
         "%26freq%3D1%26compidx%3Dnone%26compind%3Dnone%26comptemptext%3DEnter%2BSymbol%2528s%2529%26comp%3Dnone%26uf%3D0%26ma%3D1%26maval%3D200%26lf%3D1%26lf2%3D512%26lf3%3D33554432%26type%3D2%26size%3D2%26style%3D1013'%20&format=json&diagnostics=true&callback="
     }
 
@@ -81,16 +77,14 @@ chartsModule.service('chartsService', function($http, $q) {
     var mm = today.getMonth() + 1; // January is 0!
     var yyyy = today.getFullYear();
 
-    // set end date
-    var endDate =
-      "%26enddate%3D" + mm + "%252F" + dd + "%252F" + yyyy;
-
-    SELECT_QUERY_PART4 = endDate;
+    // set end date query string
+    var endDate = "%26enddate%3D" + mm + "%252F" + dd + "%252F" + yyyy;
 
     var url = YAHOO_QUERYAPI_URL +
       SELECT_QUERY_PART1 + symbol +
       SELECT_QUERY_PART2 + symbol +
-      SELECT_QUERY_PART3 + SELECT_QUERY_PART4 + SELECT_QUERY_PART5;
+      SELECT_QUERY_PART3 + endDate +
+      SELECT_QUERY_PART4;
 
     return $http.get(url).then(function (response) {
       var charts = [];
