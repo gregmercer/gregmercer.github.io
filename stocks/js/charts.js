68b18255b762b38bfe05b31b572cde8ec7c57ffa
Greg Mercer (GBear) <gmercer@stanford.edu>
2015-01-11 03:20:11 -0800

    Breaking out query url to handle enddate

--- stocks/js/charts.js
+++ stocks/js/charts.js
@@ -55,7 +55,11 @@ chartsModule.service('chartsService', function($http, $q) {
     var SELECT_QUERY_PART2 =
       "%2Fprintchart%3Fsymb%3D";
     var SELECT_QUERY_PART3 =
-      "%26countrycode%3DUS%26time%3D8%26startdate%3D1%252F4%252F1999%26enddate%3D1%252F10%252F2015%26freq%3D1%26compidx%3Dnone%26compind%3Dnone%26comptemptext%3DEnter%2BSymbol%2528s%2529%26comp%3Dnone%26uf%3D0%26ma%3D1%26maval%3D200%26lf%3D1%26lf2%3D16777216%26lf3%3D33554432%26type%3D2%26size%3D2%26style%3D1013'%20&format=json&diagnostics=true&callback=";
+      "%26countrycode%3DUS%26time%3D8%26startdate%3D1%252F4%252F1999";
+    var SELECT_QUERY_PART4 =
+      "%26enddate%3D1%252F10%252F2015";
+    var SELECT_QUERY_PART5 =
+      "%26freq%3D1%26compidx%3Dnone%26compind%3Dnone%26comptemptext%3DEnter%2BSymbol%2528s%2529%26comp%3Dnone%26uf%3D0%26ma%3D1%26maval%3D200%26lf%3D1%26lf2%3D16777216%26lf3%3D33554432%26type%3D2%26size%3D2%26style%3D1013'%20&format=json&diagnostics=true&callback=";
 
     // Funds/Etfs have a slightly different query
     if (isFund) {
@@ -64,11 +68,17 @@ chartsModule.service('chartsService', function($http, $q) {
       SELECT_QUERY_PART2 =
         "%2Fprintchart%3Fsymb%3D";
       SELECT_QUERY_PART3 =
-        "%26countrycode%3DUS%26time%3D8%26startdate%3D1%252F4%252F1999%26enddate%3D1%252F10%252F2015%26freq%3D1%26compidx%3Dnone%26compind%3Dnone%26comptemptext%3DEnter%2BSymbol%2528s%2529%26comp%3Dnone%26uf%3D0%26ma%3D1%26maval%3D200%26lf%3D1%26lf2%3D512%26lf3%3D33554432%26type%3D2%26size%3D2%26style%3D1013'%20&format=json&diagnostics=true&callback="
+        "%26countrycode%3DUS%26time%3D8%26startdate%3D1%252F4%252F1999";
+      var SELECT_QUERY_PART4 =
+        "%26enddate%3D1%252F10%252F2015";
+      var SELECT_QUERY_PART5 =
+        "%26freq%3D1%26compidx%3Dnone%26compind%3Dnone%26comptemptext%3DEnter%2BSymbol%2528s%2529%26comp%3Dnone%26uf%3D0%26ma%3D1%26maval%3D200%26lf%3D1%26lf2%3D512%26lf3%3D33554432%26type%3D2%26size%3D2%26style%3D1013'%20&format=json&diagnostics=true&callback="
     }
 
     var url = YAHOO_QUERYAPI_URL +
-      SELECT_QUERY_PART1 + symbol + SELECT_QUERY_PART2 + symbol + SELECT_QUERY_PART3;
+      SELECT_QUERY_PART1 + symbol +
+      SELECT_QUERY_PART2 + symbol +
+      SELECT_QUERY_PART3 + SELECT_QUERY_PART4 + SELECT_QUERY_PART5;
 
     return $http.get(url).then(function (response) {
       var charts = [];
