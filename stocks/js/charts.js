76f5dac250a722616485d60c0ceb5c04f2351f12
Greg Mercer (GBear) <gmercer@stanford.edu>
2015-01-11 03:06:06 -0800

    Adjustment for funds/etfs vs stocks.

--- stocks/js/charts.js
+++ stocks/js/charts.js
@@ -48,6 +48,7 @@ chartsModule.service('chartsService', function($http, $q) {
 
     // get 'printer-friendly version of this page'
     // http://www.marketwatch.com/investing/stock/ibm/charts?symb=IBM&countrycode=US&time=8&startdate=1%2F4%2F1999&enddate=1%2F10%2F2015&freq=1&compidx=none&compind=none&comptemptext=Enter+Symbol%28s%29&comp=none&uf=0&ma=1&maval=200&lf=1&lf2=16777216&lf3=33554432&type=2&size=2&style=1013
+    // http://www.marketwatch.com/investing/fund/vb/printchart?symb=VB&countrycode=US&time=8&startdate=1%2F4%2F1999&enddate=1%2F10%2F2015&freq=1&compidx=none&compind=none&comptemptext=Enter+Symbol%28s%29&comp=none&uf=0&ma=1&maval=200&lf=1&lf2=512&lf3=33554432&type=2&size=2&style=1013
 
     var SELECT_QUERY_PART1 =
       "q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fwww.marketwatch.com%2Finvesting%2Fstock%2F";
@@ -59,16 +60,15 @@ chartsModule.service('chartsService', function($http, $q) {
     // Funds/Etfs have a slightly different query
     if (isFund) {
       SELECT_QUERY_PART1 =
-        "q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fwww.marketwatch.com%2Finvesting%2Ffund%2F";
+        "q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fwww.marketwatch.com%2Finvesting%2Ffund%2F"
       SELECT_QUERY_PART2 =
         "%2Fprintchart%3Fsymb%3D";
       SELECT_QUERY_PART3 =
-        "%26countrycode%3DUS%26time%3D8%26startdate%3D1%252F4%252F1999%26enddate%3D1%252F10%252F2015%26freq%3D1%26compidx%3Dnone%26compind%3Dnone%26comptemptext%3DEnter%2BSymbol%2528s%2529%26comp%3Dnone%26uf%3D0%26ma%3D1%26maval%3D200%26lf%3D1%26lf2%3D512%26lf3%3D33554432%26type%3D2%26size%3D2%26style%3D1013'%20&format=json&diagnostics=true&callback=";
+        "%26countrycode%3DUS%26time%3D8%26startdate%3D1%252F4%252F1999%26enddate%3D1%252F10%252F2015%26freq%3D1%26compidx%3Dnone%26compind%3Dnone%26comptemptext%3DEnter%2BSymbol%2528s%2529%26comp%3Dnone%26uf%3D0%26ma%3D1%26maval%3D200%26lf%3D1%26lf2%3D512%26lf3%3D33554432%26type%3D2%26size%3D2%26style%3D1013'%20&format=json&diagnostics=true&callback="
     }
 
     var url = YAHOO_QUERYAPI_URL +
-      SELECT_QUERY_PART1 + symbol + SELECT_QUERY_PART2 + symbol + SELECT_QUERY_PART3
-      QUERY_PROPERTIES;
+      SELECT_QUERY_PART1 + symbol + SELECT_QUERY_PART2 + symbol + SELECT_QUERY_PART3;
 
     return $http.get(url).then(function (response) {
       var charts = [];
