3b4d1229a6e2fd70e94b0adc26724851f4b59932
Greg Mercer <gmercer@stanford.edu>
2022-08-11 06:05:06 -0700

    Add files via upload

---
+++ testing/testpage-1_files/js_us_v4n1JJuAp_ycW6g-y9LsUtSdV4_Zs_dWnYlfdXUc.js
@@ -0,0 +1,2 @@
+;if(window.jQuery){jQuery.noConflict()};document.documentElement.className+=' js';(function(e,n){var t=function(e){var n=function t(){e();document.removeEventListener('DOMContentLoaded',t)};if(document.readyState!=='loading'){setTimeout(e,0)}
+else{document.addEventListener('DOMContentLoaded',n)}};t(function(){e.attachBehaviors(document,n)})})(Drupal,window.drupalSettings);;
