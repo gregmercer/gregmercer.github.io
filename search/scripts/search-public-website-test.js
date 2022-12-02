67b8b06263dc72bbbaa4d211c4d15ee678cdeaa7
gmercer <gmercer@stanford.edu>
2022-12-02 05:48:34 -0800

    Adding search app test.

---
+++ search/scripts/search-public-website-test.js
@@ -0,0 +1,42 @@
+// Update this variable to point to your domain.
+var apigatewayendpoint = 'https://t4s806qq1a.execute-api.us-west-2.amazonaws.com/opensearch-api-public-site-test/opensearch-public-site-lambda';
+var loadingdiv = $('#loading');
+var noresults = $('#noresults');
+var resultdiv = $('#results');
+var searchbox = $('input#search');
+var timer = 0;
+
+// Executes the search function 250 milliseconds after user stops typing
+searchbox.keyup(function () {
+  clearTimeout(timer);
+  timer = setTimeout(search, 250);
+});
+
+async function search() {
+  // Clear results before searching
+  noresults.hide();
+  resultdiv.empty();
+  loadingdiv.show();
+  // Get the query from the user
+  let query = searchbox.val();
+  // Only run a query if the string contains at least three characters
+  if (query.length > 2) {
+    // Make the HTTP request with the query as a parameter and wait for the JSON results
+    let response = await $.get(apigatewayendpoint, { q: query, size: 25 }, 'json');
+    // Get the part of the JSON response that we care about
+    let results = response['hits']['hits'];
+    console.log(results);
+    if (results.length > 0) {
+      loadingdiv.hide();
+      }
+    } else {
+      noresults.show();
+    }
+  }
+  loadingdiv.hide();
+}
+
+// Tiny function to catch images that fail to load and replace them
+function imageError(image) {
+  image.src = 'images/no-image.png';
+}
